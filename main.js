865b93763182267ba47414bba99f938cd8405ef8
silvestrevivo <silvestrevivomillan@gmail.com>
2020-02-12 09:50:40 +0100

    destop version

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('CSS Microsoft HomePage');
+const menuButton = document.querySelector('.menu-btn');
+const mainMenu = document.querySelector('.main-menu');
+
+menuButton.addEventListener('click', () => mainMenu.classList.toggle('show'));
